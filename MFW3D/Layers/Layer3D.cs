6deb316a665b311e86a780dc9893f2f23bda6a6a
毛自豪 <captainl1993@126.com>
2017-11-14 18:27:46 +0800

    更新
    Signed-off-by: 毛自豪 <captainl1993@126.com>

---
+++ MFW3D/Layers/Layer3D.cs
@@ -0,0 +1,41 @@
+﻿using MFW3D.Renderable;
+using Microsoft.DirectX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MFW3D.Layers
+{
+    public class Layer3D : RenderableObject
+    {
+        public Layer3D(string name,
+            float latitude,
+            float longitude,
+            float layerRadius,
+            float scaleFactor,
+            string meshFilePath,
+            Quaternion orientation) : base(name, 
+                MathEngine.SphericalToCartesian(latitude, longitude, layerRadius), orientation) 
+        {
+
+        }
+
+        public override void Initialize(DrawArgs drawArgs)
+        { }
+
+        public override void Update(DrawArgs drawArgs)
+        { }
+
+        public override void Render(DrawArgs drawArgs)
+        { }
+        public override void Dispose()
+        {
+
+        }
+        public override bool PerformSelectionAction(DrawArgs drawArgs)
+        {
+            return true;
+        }
+    }
+}
